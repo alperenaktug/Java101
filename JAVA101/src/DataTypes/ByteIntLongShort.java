7014c2d0de017ab8bc701d890fd8a5b6092e31b7
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-03-30 14:26:16 +0300

    ByteShortIntLong

---
+++ JAVA101/src/DataTypes/ByteIntLongShort.java
@@ -0,0 +1,14 @@
+package DataTypes;
+
+public class ByteIntLongShort {
+    public static void main(String[] args) {
+        byte vByte = 100;
+        short vShort = 1000;
+        int vInt = 10000;
+        long vLong = 1000000;
+        System.out.println(vByte);
+        System.out.println(vShort);
+        System.out.println(vInt);
+        System.out.println(vLong);
+    }
+}
