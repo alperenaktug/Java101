358d47b3a3476395d73958c8692223d4ef5eead5
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-03-30 13:14:34 +0300

    escape ifadeler

--- JAVA101/src/Giris/Baslangic.java
+++ JAVA101/src/Giris/Baslangic.java
@@ -2,6 +2,13 @@ package Giris;
 
 public class Baslangic {
     public static void main(String[] args) {
-        System.out.print("Hello World !");
+        System.out.println("Hello World !");
+        System.out.println("Java 101 beginner");
+
+        System.out.println("I am always \n in the top ");// escape character
+        System.out.println("You are mad\t\tIam back ");
+        System.out.println("You are mad\rI am back ");
+
+        System.out.println("Say my name \"Heisenberg\"");
     }
 }
