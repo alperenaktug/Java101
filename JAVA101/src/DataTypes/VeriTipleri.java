e8302f1d12f3bdd54988f843a077a69574b110fd
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-03-30 14:11:49 +0300

    data types information

---
+++ JAVA101/src/DataTypes/VeriTipleri.java
@@ -0,0 +1,10 @@
+package DataTypes;
+
+public class VeriTipleri {
+    public static void main(String[] args) {
+        int numberOne , numberTwo = 2 , numberThree;
+        numberOne = 1 ;
+        numberThree = numberOne + numberTwo ;
+        System.out.println(numberThree);
+    }
+}
