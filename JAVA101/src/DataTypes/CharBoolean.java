125e64b7e2abbeff6c71ec0ac73d5faba17090fb
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-03-30 14:48:47 +0300

    char boolean

---
+++ JAVA101/src/DataTypes/CharBoolean.java
@@ -0,0 +1,27 @@
+package DataTypes;
+
+import java.sql.SQLOutput;
+
+public class CharBoolean {
+    public static void main(String[] args) {
+        char v1 = 'J';
+        char v2 = 'A';
+        char v3 = 'V';
+        char v4 = 'A';
+
+        System.out.println(  v1 +v2 + v3 + v4);
+        System.out.println(v1);
+        System.out.println(v2);
+        System.out.println(v3);
+        System.out.println(v4);
+
+
+        boolean dogru = true ;
+        boolean yanlis = false;
+
+        System.out.println(dogru);
+        System.out.println(yanlis);
+
+
+    }
+}
