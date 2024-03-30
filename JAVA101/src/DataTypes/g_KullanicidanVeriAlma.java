1da063b8ef0492c705ebd3c2736e675483c93f2c
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-03-30 17:48:03 +0300

    scanner sınıfı veri girişi

--- JAVA101/src/DataTypes/g_KullanicidanVeriAlma.java
+++ JAVA101/src/DataTypes/g_KullanicidanVeriAlma.java
@@ -1,7 +1,29 @@
 package DataTypes;
-
+import java.util.Scanner;
 public class g_KullanicidanVeriAlma {
     public static void main(String[] args) {
+     Scanner input = new Scanner(System.in);
+
+        int a , b ;
+        System.out.println("A sayısını giriniz :");
+        System.out.println("B sayısını giriniz :");
+        a = input.nextInt();
+        b = input.nextInt();
+
+        int toplam = a + b;
+        int fark = a - b;
+        int carpma = a * b;
+        int bolme = a / b;
+
+
+        System.out.println("A sayısı : " + a);
+        System.out.println("B sayısı : " + b);
+
+        System.out.println("A ile B sayısının toplamı : " + toplam);
+        System.out.println("A ile B sayısının farkı : " + fark);
+        System.out.println("A ile B sayısının çarpımı : " + carpma);
+        System.out.println("A ile B sayısının bölümü : " + bolme);
+
 
     }
 }
