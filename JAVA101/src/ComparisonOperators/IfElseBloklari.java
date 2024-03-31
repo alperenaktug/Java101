8f808fefd436c7a462de052363f97dfe42e3c5e0
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-03-31 18:18:32 +0300

    if else komutları

---
+++ JAVA101/src/ComparisonOperators/IfElseBloklari.java
@@ -0,0 +1,27 @@
+package ComparisonOperators;
+
+public class IfElseBloklari {
+    public static void main(String[] args) {
+        int a=10 , b=20 ,  c=30;
+        boolean compare = a==b;
+
+        // String str = compare ? "Eşittir" : "Eşit değildir";
+        // System.out.println(str);
+
+        if (compare){
+            System.out.println("Eşittir");
+        }else{
+            System.out.println("Eşit değildir");
+        }
+        System.out.println("Program bitti!!");
+
+
+        if((a < b) && (a < c)){
+            System.out.println("A en küçük sayıdır.");
+        }else if((b <a) && (b < c)){
+            System.out.println("B en küçük sayıdır.");
+        }else{
+            System.out.println("C en küçük sayıdır.");
+        }
+    }
+}
