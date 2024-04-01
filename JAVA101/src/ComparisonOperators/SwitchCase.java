c618921d99e198d1806d391380d9da37d2e7cae9
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-04-01 16:02:12 +0300

    switch case

---
+++ JAVA101/src/ComparisonOperators/SwitchCase.java
@@ -0,0 +1,40 @@
+package ComparisonOperators;
+
+public class SwitchCase {
+    public static void main(String[] args) {
+
+        int days = 5 ;
+
+        switch (days){
+
+            case 1 :
+                System.out.println("Bugün günlerden pazartesi");
+                break;
+            case 2 :
+                System.out.println("Bugün günlerden salı");
+                break;
+            case 3 :
+                System.out.println("Bugün günlerden çarşamba");
+                break;
+            case 4 :
+                System.out.println("Bugün günlerden perşembe");
+                break;
+            case 5 :
+                System.out.println("Bugün günlerden cuma");
+                break;
+            case 6 :
+                System.out.println("Bugün günlerden cumartesi");
+                break;
+            case 7 :
+                System.out.println("Bugün günlerden pazar");
+                break;
+
+
+
+
+        }
+
+
+
+    }
+}
