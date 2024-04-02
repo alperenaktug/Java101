f11c445204f6111a91cc3cb915fbe4dba04c1f6f
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-04-02 12:29:29 +0300

    break ve continue kavramları

---
+++ JAVA101/src/Loops/BreakContinue.java
@@ -0,0 +1,22 @@
+package Loops;
+
+public class BreakContinue {
+    public static void main(String[] args) {
+
+        for (int i = 1 ; i <= 10 ; i++){
+
+            if (i == 5 ){
+                System.out.println("i değeri 5 tir ...");
+                break;
+            }
+            System.out.println(i);
+        }
+
+        for (int k=1 ; k <= 10 ; k++){
+            if (k == 5){
+                continue;
+            }
+            System.out.println(k);
+        }
+    }
+}
