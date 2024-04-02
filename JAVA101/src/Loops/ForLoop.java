21f6728297a3f4dd9e5a04c3d5005fa24b107ec7
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-04-02 12:09:00 +0300

    for loop

---
+++ JAVA101/src/Loops/ForLoop.java
@@ -0,0 +1,9 @@
+package Loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        for (int sayac = 1 ; sayac <= 10 ; sayac++){
+            System.out.println(sayac);
+        }
+    }
+}
